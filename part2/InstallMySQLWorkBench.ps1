c27fe54339e432a079038874265262d890f01b14
Spark-Aspire2 <aeu3000774@aspire2student.ac.nz>
2024-09-08 00:14:31 +1200

    Part 2 finished.

--- part2/InstallMySQLWorkBench.ps1
+++ part2/InstallMySQLWorkBench.ps1
@@ -4,7 +4,7 @@
 # 1. Download and Install the Latest Microsoft Visual C++ Redistributable
 $vc_redist = "https://aka.ms/vs/17/release/vc_redist.x64.exe"
 $vc_redistInstaller = "C:\Downloads\vc_redist.x64.exe"
-#Invoke-WebRequest -Uri $vc_redist -OutFile $vc_redistInstaller -TimeoutSec 30
+Invoke-WebRequest -Uri $vc_redist -OutFile $vc_redistInstaller -TimeoutSec 30
 
 Start-Process -FilePath $vc_redistInstaller  -ArgumentList "/quiet", "/norestart" -Wait
 
@@ -13,7 +13,7 @@ Start-Process -FilePath $vc_redistInstaller  -ArgumentList "/quiet", "/norestart
 # 2. Download and Install the latest mysql-workbench
 $mysqlwb = "https://cdn.mysql.com//Downloads/MySQLGUITools/mysql-workbench-community-8.0.38-winx64.msi"
 $mysqlwbInstaller = "C:\Downloads\mysql-workbench-community-8.0.38-winx64.msi"
-#Invoke-WebRequest -Uri $mysqlwb -OutFile $mysqlwbInstaller -TimeoutSec 30
+Invoke-WebRequest -Uri $mysqlwb -OutFile $mysqlwbInstaller -TimeoutSec 30
 
 Start-Process -FilePath "msiexec.exe" -ArgumentList "/i", $mysqlwbInstaller, "/quiet", "/norestart" -Wait
 
